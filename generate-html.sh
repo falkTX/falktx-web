cedadd1ef43307b277c6548b26438f7547f7c0f0
falkTX <falktx@gmail.com>
2014-03-10 21:07:12 +0000

    Update generator

--- generate-html.sh
+++ generate-html.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+mkdir -p gen
+
 # base
-php index.php > index.html
+php index.php home 0 > gen/index.html
 
 # pages
 PAGES="home myself projects social cv"
 
 for page in $PAGES; do
   echo $page;
-  php index.php $page 0 > "index.php?page=$page"
+  php index.php $page 0 > "gen/$page.html"
 done
 
 # anim
-php index.php home 1 > "index.php?anim=1"
-php index.php home 1 > "index.php?page=home&anim=1"
+php index.php home 1 > "gen/index+a.html"
